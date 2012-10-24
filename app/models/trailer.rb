f547cfca1f390abc4eec0d1f47a9ed1eb3fcc0f7
Kyle Sotello <ksotello@uncc.edu>
2012-10-24 19:34:14 -0400

    add view movie trailer

--- app/models/trailer.rb
+++ app/models/trailer.rb
@@ -2,6 +2,9 @@ class Trailer < ActiveRecord::Base
   # attr_accessible :title, :body
   def self.retrieve_movie_trailer(movie)
   	@trailer = Youtube::Video.find(:params => {:q => "#{movie}"+"trailer", :v => '2'})
+  end
+
+  def self.view_movie_trailer
   	@trailer = @trailer.entry[0].link[0].href
   end
 end
